e7ca99edd389570baca2df9e16c703d11cde8418
ygtzz <ygtzz@126.com>
2018-05-28 14:13:46 +0800

    add lazyload test

--- src/pages/index/views/test.vue
+++ src/pages/index/views/test.vue
@@ -1,41 +1,46 @@
 <template>
     <div class="test stage">
         <h2>test</h2>
-        <div class="slider2c">
-            <c-slider class="slider" :loop="true" :dir="dir">
-                <c-slider-item v-for="n in list" :key="'slider' + n">
-                    <div class="slide">{{n}}</div>
-                </c-slider-item>
-            </c-slider>
+        <div class="imgc" style="margin-top:0">
+            <img src="" data-src="/static/images/1.jpg" alt="">
+            <img src="" data-src="/static/images/2.jpg" alt="">
+            <img src="" data-src="/static/images/3.jpg" alt="">
+        </div>
+        <div class="imgc">
+            <img src="" data-src="/static/images/4.jpg" alt="">
+            <img src="" data-src="/static/images/5.jpg" alt="">
+            <img src="" data-src="/static/images/6.jpg" alt="">
+        </div>
+        <div class="imgc horizontal">
+            <img src="" data-src="/static/images/7.jpg" alt="">
+            <img src="" data-src="/static/images/8.jpg" data-defer="3000" alt="">
         </div>
     </div>
 </template>
 <style lang="sass" scoped>
-    //height required
-    .slider{height:300px;background-image:url('http://pic2.ooopic.com/10/55/95/20b1OOOPICfa.jpg');background-size:cover;}
-    .slide{width:100%;height:300px;text-align:center;font-size:80px;line-height:300px;}
-    .slide:nth-child(1){background-color:pink;}
-    .slide:nth-child(2){background-color:lightgreen;}
-    .slide:nth-child(3){background-color:pink;}
-    .slider2c{
-        margin-top:50px;
+   .imgc{
+        margin-top:250px;
     }
+    img{width:300px;height:200px;display:block;margin-top:50px;}
+    .horizontal{overflow: hidden;}
+    .horizontal *{float:left;margin-left:50px;}
+    .horizontal :first-child{margin-left:0;}
 </style>
 <script>
 import Vue from 'vue';
-import 'vue-countdown2/dist/vue-countdown.css';
-// in ES6 modules
-import { countdown } from 'vue-countdown2';
-import { slider, sliderItem } from 'index/widget/slider';
-
-Vue.component('c-countdown',countdown);
+import {LazyLoad} from 'my-lazyload';
 
 export default {
     name:'v-test',
     created() {
-        setTimeout(() => {
-            this.list = [1,2,3,4];
-        },3000);
+        
+    },
+    mounted(){
+        console.log(LazyLoad);
+        new LazyLoad({
+            imgSelector:'img[data-src]',
+            threshold: 250
+        });
     },
     data() {
       return {
@@ -57,8 +62,7 @@ export default {
         
     },
     components:{
-        'c-slider': slider,
-        'c-slider-item': sliderItem
+        
     },
     filters:{
         
